8ce2f7d6d6e070967b89ccd503b78f04ec14f0fd
Jp90_22 <joseotnielp@outlook.es>
2022-10-27 15:55:43 -0400

    Analytics implmented and added a reusable styles file

--- src/app/components/Analytics.jsx
+++ src/app/components/Analytics.jsx
@@ -1,5 +1,91 @@
 import React from "react";
+import styled from "styled-components";
+import { cardStyles } from "./reusableStyles";
 
+// Icons
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import { IoStatsChart } from "react-icons/io5";
+import { BiGroup } from "react-icons/bi";
+import { FiActivity } from "react-icons/fi";
+
+/**
+ * Analytic styled component
+ * @returns Analytics component
+ */
 export default function Analytics() {
-  return <div>Analytics</div>;
+  return (
+    <Section>
+      <div className="analytic">
+        <div className="logo">
+          <BsFillCalendar2WeekFill />
+        </div>
+        <div className="content">
+          <h5>Spent this month</h5>
+          <h2>$785.30</h2>
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="logo">
+          <IoStatsChart />
+        </div>
+        <div className="content">
+          <h5>Earnings</h5>
+          <h2>$445.00</h2>
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="logo">
+          <BiGroup />
+        </div>
+        <div className="content">
+          <h5>New clients</h5>
+          <h2>223</h2>
+        </div>
+      </div>
+      <div className="analytic">
+        <div className="logo">
+          <FiActivity />
+        </div>
+        <div className="content">
+          <h5>Activity</h5>
+          <h2>$597.29</h2>
+        </div>
+      </div>
+    </Section>
+  );
 }
+
+/**
+ * Section styled component
+ */
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  .analytic {
+    ${cardStyles};
+    padding: 1rem;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    gap: 1rem;
+    transition: 0.5s ease-in-out;
+    &:hover {
+      background-color: #ffc107;
+      .content {
+        color: black;
+      }
+    }
+    .logo {
+      background-color: black;
+      border-radius: 3rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 1.5rem;
+      svg {
+        font-size: 1.5rem;
+      }
+    }
+  }
+`;
--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -21,8 +21,30 @@ export default function Dashboard() {
   );
 }
 
+/**
+ * Section styled component
+ */
 const Section = styled.section`
   margin-left: 18vw;
   padding: 2rem;
   height: 100%;
+  .grid {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    gap: 1rem;
+    margin-top: 2rem;
+    .row__one {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      height: 50%;
+      gap: 1rem;
+    }
+    .row__two {
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      gap: 1rem;
+      height: 50%;
+    }
+  }
 `;
